899a04d20334f96d67f4b6b1f8a3f86815e567be
wychoong <67364036+wychoong@users.noreply.github.com>
2023-10-16 13:44:59 +0800

    add pages

---
+++ app/Filament/Pages/Test1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Test1 extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.test1';
+}
