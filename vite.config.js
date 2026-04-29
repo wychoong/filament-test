e1f4cbd6f6a8fa70ffc425e1eb83170d4bfb8c13
wychoong <67364036+wychoong@users.noreply.github.com>
2026-04-29 17:49:49 +0800

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -12,5 +13,11 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
